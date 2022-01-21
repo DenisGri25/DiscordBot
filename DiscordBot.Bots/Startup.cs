8b36044c3a8dd9757e379a9d4fc642a7015beade
Денис Григорьев <Denisgri25@outlook.com>
2022-01-21 17:11:16 +0300

    Add project files.

---
+++ DiscordBot.Bots/Startup.cs
@@ -0,0 +1,37 @@
+﻿using DiscordBot.Core.Services.Items;
+using DiscordBot.Core.Services.Profiles;
+using DiscordBot.DAL;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DiscordBot.Bots
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<RPGContext>(options =>
+            {
+                options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=RPGContext;Trusted_Connection=True;MultipleActiveResultSets=true",
+                    x => x.MigrationsAssembly("DiscordBot.DAL.Migrations"));
+                options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            });
+
+            services.AddScoped<IItemService, ItemService>();
+            services.AddScoped<IExperienceService, ExperienceService>();
+            services.AddScoped<IProfileService, ProfileService>();
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            var bot = new Bot(serviceProvider);
+            services.AddSingleton(bot);
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+        }
+    }
+}
